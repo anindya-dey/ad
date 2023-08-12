11fbfc794169a673031a283eb123a49da004ca6d
Anindya Dey <30517208+anindya-dey@users.noreply.github.com>
2023-08-12 16:11:26 +0530

    upgrade Blog (#10)
    * setup blog
    
    * setup tailwindcss
    
    * update packages
    
    * add tailwind css to portfolio
    
    * update default test case in blog
    
    * update default test case in blog
    
    * setup github ci
    
    * update favicon
    
    * migrate nx to latest version (16.5.0)
    
    * update github workflow ci
    
    * upgrade packages
    
    * add navbar to blog
    
    * upgrade pacakages
    
    * upgrade blog
    
    * update github ci workflow
    
    * replace npx with pnpm in ci workflow
    
    * format and update ci
    
    ---------
    
    Co-authored-by: Anindya Dey <anindya.dey@vista.com>

--- apps/portfolio/app/page.tsx
+++ apps/portfolio/app/page.tsx
@@ -1,5 +1,7 @@
 export default async function Index() {
   return (
-    <div className="text-5xl" data-cy="page-title">Portfolio</div>
+    <div className="text-5xl" data-cy="page-title">
+      Portfolio
+    </div>
   );
 }
