d54dd7adee96bf75253ab4dab272a71547894bd2
Anindya Dey <andy.ani1992@gmail.com>
2023-10-26 20:13:26 +0530

    portfolio: dismiss mobile menu on click

--- apps/portfolio/components/Navbar.tsx
+++ apps/portfolio/components/Navbar.tsx
@@ -37,11 +37,11 @@ const navItems = [
 
 export default function NavBar() {
   const path = usePathname();
-  const { isMobileMenuOpen } = useContext(MobileMenuContext);
+  const { isMobileMenuOpen, setIsMobileMenuOpen } = useContext(MobileMenuContext);
 
   return (
     <nav className="fixed top-0 flex flex-col w-full md:static">
-      <div className="flex justify-between w-full px-6 py-2 shadow-sm md:px-0 md:py-8 md:shadow-none dark:border-b dark:md:border-none dark:border-zinc-800 bg-white/30 backdrop-blur-sm md:backdrop-blur-none md:bg-transparent dark:bg-zinc-700/20 md:dark:bg-transparent">
+      <div className="flex justify-between w-full px-6 py-2 shadow-sm bg-white/30 md:px-0 md:py-8 md:shadow-none dark:border-b dark:md:border-none dark:border-zinc-800 backdrop-blur-sm md:backdrop-blur-none md:bg-transparent dark:bg-zinc-700/20 md:dark:bg-transparent">
         <div>
           <Link href={'/'}>
             <Image src={'/logo.png'} alt="logo" width={64} height={64} />
@@ -67,12 +67,16 @@ export default function NavBar() {
         </div>
       </div>
       {isMobileMenuOpen ? (
-        <div className="flex flex-col justify-between p-6 shadow-md bg-white/30 backdrop-blur-lg md:backdrop-blur-none md:bg-transparent dark:bg-zinc-700/20 md:dark:bg-transparent md:hidden dark:border-b dark:border-zinc-800">
+        <div className="flex flex-col justify-start min-h-screen p-6 bg-zinc-700/80 backdrop-blur-sm md:backdrop-blur-none md:bg-transparent dark:bg-zinc-900/30 md:dark:bg-transparent md:hidden dark:border-b dark:border-zinc-900" onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
           {navItems.map((navItem) => (
             <Link key={navItem.name} href={navItem.href} prefetch>
               <div
                 key={navItem.name}
-                className="flex items-center my-3 font-medium text-zinc-900/70 dark:text-white/70"
+                className={
+                  path === navItem.href
+                  ? "flex items-center my-2 font-medium px-6 py-3 rounded-lg shadow-md text-white dark:text-white/70 bg-gradient-to-r from-[#FA5252] to-[#DD2476]"
+                  : "flex items-center my-2 font-medium px-6 py-3 rounded-lg text-gray-700 dark:text-white/70 bg-white/70 dark:text-zinc-200 dark:bg-zinc-800"
+                }
               >
                 <span className="mr-4 text-lg">{navItem.icon}</span>
                 <span className="text-md">{navItem.name}</span>
