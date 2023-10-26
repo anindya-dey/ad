1cb262f62057e9ef47398d5a95fcccdc09f408e3
Anindya Dey <andy.ani1992@gmail.com>
2023-10-26 17:56:49 +0530

    portfolio: add padding-x for iPad view

--- apps/portfolio/app/layout.tsx
+++ apps/portfolio/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning={true}>
       <body className="w-full h-screen bg-fixed bg-center bg-no-repeat bg-cover bg-homeBg dark:bg-homeBg-dark font-poppins">
         <Providers>
-          <div className="container">
+          <div className="container md:px-6">
             <NavBar />
             <main className="flex flex-col items-center mt-6 md:mt-0 md:border-white md:border md:drop-shadow-md md:bg-white/5 md:rounded-2xl md:backdrop-blur-sm dark:md:border-zinc-800">
               {children}
