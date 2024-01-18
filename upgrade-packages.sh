343375bb064fda77d01ca0d47aed36ecfbfa8e9b
Anindya Dey <anindya.dey@vista.com>
2024-01-18 15:16:51 +0530

    upgrade packages

--- upgrade-packages.sh
+++ upgrade-packages.sh
@@ -1,3 +1,9 @@
+echo "Creating new branch..."
+
+git switch -c upgrade
+
+echo "New branch created!"
+
 echo "Installing dependencies..."
 
 pnpm add @nx/next next next-themes react react-dom react-icons
@@ -9,3 +15,17 @@ echo "Installing dev-dependencies..."
 pnpm add --save-dev @nx/cypress @nx/eslint-plugin @nx/jest @nx/js @nx/linter @nx/react @nx/workspace @testing-library/react @types/jest @types/node @types/react @types/react-dom @typescript-eslint/eslint-plugin @typescript-eslint/parser autoprefixer babel-jest cypress eslint eslint-config-next eslint-config-prettier eslint-plugin-cypress eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react eslint-plugin-react-hooks jest jest-environment-jsdom jest-environment-node nx nx-cloud postcss prettier tailwindcss ts-jest ts-node tslib typescript 
 
 echo "Installed dev-dependencies!"
+
+echo "Commiting files..."
+
+git add .
+git commit -m "upgrade packages"
+
+echo "Files committed!"
+
+echo "Pushing changes..."
+
+git push -u origin upgrade
+
+echo "Changes pushed!"
+
