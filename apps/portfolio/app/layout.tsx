3e0717ca144e8560c266b06de9dc7adb3c295398
Anindya Dey <andy.ani1992@gmail.com>
2024-02-04 15:17:46 +0530

    remove fixed position

--- apps/portfolio/app/layout.tsx
+++ apps/portfolio/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
             <NavBar />
             <main className="flex flex-col mt-6 md:mt-0 md:border-white md:border md:drop-shadow-md md:bg-white/40 dark:md:bg-white/5 md:rounded-2xl md:backdrop-blur-sm dark:md:border-zinc-800 overflow-clip">
               {children}
-              <div className="fixed bottom-0 left-0 right-0 flex justify-center py-6 text-sm font-thin text-gray-600 bg-teal-100/20 dark:bg-white/10 dark:text-gray-100 backdrop-blur-sm">
+              <div className="flex justify-center py-6 text-sm font-thin text-gray-600 bg-teal-100/20 dark:bg-white/10 dark:text-gray-100 backdrop-blur-sm">
                 &copy; 2024 Anindya Dey
               </div>
             </main>
