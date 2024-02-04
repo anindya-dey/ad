adc4be992e4f37f3987198ad4ba034334d51ea10
Anindya Dey <anindya.dey@vista.com>
2024-02-04 12:48:20 +0530

    setup about page

--- apps/portfolio/app/about/page.tsx
+++ apps/portfolio/app/about/page.tsx
@@ -5,5 +5,16 @@ export const metadata: Metadata = {
   }
 
 export default function Page() {
-  return <div>About</div>
+  return (
+    <div className="flex flex-col px-6 py-24 md:py-10">
+      <div className="text-4xl font-medium text-gray-900 md:text-4xl dark:text-white">About Me</div>
+      <div className="flex flex-row justify-evenly">
+        <div>image</div>
+        <div className="flex flex-col">
+          <div>Who am I?</div>
+          <div>Personal Info</div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- apps/portfolio/app/layout.tsx
+++ apps/portfolio/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
         <Providers>
           <div className="container md:px-10">
             <NavBar />
-            <main className="flex flex-col items-center mt-6 md:mt-0 md:border-white md:border md:drop-shadow-md md:bg-white/5 md:rounded-2xl md:backdrop-blur-sm dark:md:border-zinc-800">
+            <main className="flex flex-col mt-6 md:mt-0 md:border-white md:border md:drop-shadow-md md:bg-white/5 md:rounded-2xl md:backdrop-blur-sm dark:md:border-zinc-800">
               {children}
             </main>
           </div>
