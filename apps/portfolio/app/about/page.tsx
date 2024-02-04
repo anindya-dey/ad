39abad8f505c3ad2205e4c6ef3867891111566d3
Anindya Dey <andy.ani1992@gmail.com>
2024-02-04 15:51:33 +0530

    add personal info section

--- apps/portfolio/app/about/page.tsx
+++ apps/portfolio/app/about/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next';
 import Image from 'next/image';
-import { FaEnvelopeOpenText, FaMapMarkerAlt } from 'react-icons/fa';
+
+import PersonalInfoSection from '../../components/PersonalInfoSection';
 
 export const metadata: Metadata = {
   title: 'About | Anindya Dey',
@@ -22,8 +23,8 @@ export default function Page() {
         />
       </div>
       <div className="mt-4">
-        <div className="text-2xl font-thin">Who am I?</div>
-        <div className="py-4 font-thin">
+        <div className="text-2xl">Who am I?</div>
+        <div className="py-4 text-gray-700 dark:text-gray-300">
           I am a goal-oriented and mission-driven full-stack software developer
           from India with 8 years+ of industry experience, and with an ability
           to self-manage during independent projects, as well as collaborate as
@@ -33,26 +34,9 @@ export default function Page() {
         </div>
       </div>
       <div className="mt-4">
-        <div className="text-2xl font-thin">Personal Info</div>
-        <div className="py-4">
-          <div className="flex">
-            <span className="p-2 mr-2 text-xl text-center transition duration-300 ease-in-out bg-white rounded-md shadow-md dark:bg-zinc-900 dark:border dark:border-zinc-700 hover:-translate-y-1 md:p-3 md:text-md">
-              <FaEnvelopeOpenText className="text-teal-400" />
-            </span>
-            <div>
-              <div className="text-xs">Email</div>
-              <div className="">anindya.dey01@gmail.com</div>
-            </div>
-          </div>
-          <div className="flex mt-4">
-            <span className="p-2 mr-2 text-xl text-center transition duration-300 ease-in-out bg-white rounded-md shadow-md dark:bg-zinc-900 dark:border dark:border-zinc-700 hover:-translate-y-1 md:p-3 md:text-md">
-              <FaMapMarkerAlt className="text-rose-400" />
-            </span>
-            <div>
-              <div className="text-xs">Location</div>
-              <div className="">Nagaland, India</div>
-            </div>
-          </div>
+        <div className="text-2xl">Personal Info</div>
+        <div>
+          <PersonalInfoSection />
         </div>
       </div>
     </div>
